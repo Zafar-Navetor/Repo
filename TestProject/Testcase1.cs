f615d72678acd88e827722310f55c5375a7710a4
Zafar Ahmad <ZAhmad@pattersoncompanies.com>
2019-10-11 17:34:40 +0500

    My commit

---
+++ TestProject/Testcase1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Testcase1
+    {
+
+        public void Login(String UserName, String Passowrd)
+        {
+            UserName = "ABC";
+            Passowrd = "ABC";
+        }
+    }
+}
